b4e8a59490b6383c025546db9652cd94aae8ebd8
Amit Malik <contact2amitmalik@gmail.com>
2018-09-19 14:21:09 -0700

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Migrating Apps to the Cloud with Azure/title>
+    </head>
+    <body>
+        <p>
+            Hello. This is sample of building docker images and running basic website built using PHP on Docker. Today is <?= date('l \t\h\e jS') ?>.
+        </p>
+    </body>
+</html>
